e8dd08ef5c5e997297db1cef4038bb5dda490ea3
jaredjones@utexas.edu <jaredjones@utexas.edu@280e46b7-98d9-0841-a3c0-699020cface8>
2015-07-17 08:26:00 +0000

    Driver Cleanup
    git-svn-id: https://agile.cs.uh.edu/svn/u15_ic/dickson_jones@878 280e46b7-98d9-0841-a3c0-699020cface8

--- assign1/src/driver.js
+++ assign1/src/driver.js
@@ -5,38 +5,13 @@ var errorCallback = function(err){
 }
 
 var responseCallback = function(weatherTupleArray){
-    console.log(weatherTupleArray);
+    console.log("City          Region  Temp");
+    weatherTupleArray.forEach(function(tuple){
+        var city = tuple[0];
+        var region = tuple[1];
+        var temp = tuple[2];
+        console.log(city + " " + region + " " + temp);
+    });
 }
 
 weather.getWeatherForCities("WOEIDS.txt", responseCallback, errorCallback); 
-
-
-/*weather.readWOEIDFile("WOEIDS.txt", function(data, error){
-    if (error){
-        return console.log('Error Reading File!');
-    }
-
-    var WOEIDArray = weather.fileStringToWOEIDArray(data);
-    var WOEIDArrayFinalized = weather.removeEmptyOrInvalidDataFromArray(WOEIDArray);
-    outputWeatherDataGivenArray(WOEIDArrayFinalized);
-});
-
-var outputWeatherDataGivenArray = function(woeidArray){
-    console.log('Please Wait (Getting Weather Data)...');
-    weather.getWeatherData(woeidArray[0], weatherDataXMLReceived);
-}
-
-var weatherDataXMLReceived = function(data){
-    var cityTuple = weather.weatherXMLToTuple(data);
-    console.log(cityTuple[0] + " " + cityTuple[1] + " " + cityTuple[2]);
-}*/
-
-
-//Venkat: Can we refactor this further to the following?
-
-//weather.getWeatherForCities("WOEIDS.txt", responseFunction, errorFunction);
-
-//where errorFunction is called if there was an error reading the file and
-//responseFunction may be called with a sorted list of weather data.
-
-//That way the driver only has to make one single call with functions that only do console.log();
