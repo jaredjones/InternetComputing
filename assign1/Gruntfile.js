43e072bbe5406d8d350cef5d6c24a2d6cd7a0132
jaredjones@utexas.edu <jaredjones@utexas.edu@280e46b7-98d9-0841-a3c0-699020cface8>
2015-07-18 09:50:25 +0000

    Grunt Now Compiles Coffee
    git-svn-id: https://agile.cs.uh.edu/svn/u15_ic/dickson_jones@1055 280e46b7-98d9-0841-a3c0-699020cface8

--- assign1/Gruntfile.js
+++ assign1/Gruntfile.js
@@ -9,10 +9,12 @@ module.exports = function(grunt) {
       }
     },
     exec: {
-      remove_include_js: 'rm src/include.js',
+      remove_include_temp: 'rm -f src/include.js; rm -rf src/temp',
       weatherjs_to_include: 'cp src/weather.js src/include.js',
+      weathercoffee_to_files: 'coffee -o src/temp -c src/driver.coffee; coffee -o src/temp -c src/weather.coffee; mv src/temp/weather.js src/include.js; mv src/temp/driver.js src/driver-coffee.js',
       coverage: 'node "node_modules/istanbul/lib/cli.js" cover "node_modules/nodeunit/bin/nodeunit" -- test',
-      run_driver: 'node src/driver.js'
+      run_driver: 'node src/driver.js',
+      run_driver_coffee: 'node src/driver-coffee.js; rm -rf src/temp'
     }
   });
 
@@ -23,6 +25,8 @@ module.exports = function(grunt) {
   grunt.registerTask('coverage', 'exec:coverage');
   grunt.registerTask('run_driver', 'exec:run_driver');
   grunt.registerTask('weatherjs_to_include', 'exec:weatherjs_to_include');
-  grunt.registerTask('remove_include_js', 'exec:remove_include_js');
-  grunt.registerTask('default', ['install-dependencies', 'weatherjs_to_include', 'nodeunit', 'coverage','run_driver']);
+  grunt.registerTask('weathercoffee_to_files', 'exec:weathercoffee_to_files');
+  grunt.registerTask('remove_include_temp', 'exec:remove_include_js');
+  grunt.registerTask('run_driver_coffee','exec:run_driver_coffee');
+  grunt.registerTask('default', ['install-dependencies', 'weatherjs_to_include', 'nodeunit', 'coverage','run_driver','weathercoffee_to_files','run_driver_coffee']);
 }
