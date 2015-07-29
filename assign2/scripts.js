6841997be6c87f6afaeaf25b8e4ab21a51ec2bb3
gteldirith543@gmail.com <gteldirith543@gmail.com@280e46b7-98d9-0841-a3c0-699020cface8>
2015-07-29 07:35:22 +0000

    Working on copy and paste box
    git-svn-id: https://agile.cs.uh.edu/svn/u15_ic/dickson_jones@2103 280e46b7-98d9-0841-a3c0-699020cface8

--- assign2/scripts.js
+++ assign2/scripts.js
@@ -33,7 +33,9 @@ var registerDragDrop = function(){
 	var whenDropped = function(event){
 		event.preventDefault();
 		var data = event.dataTransfer.getData("text");
-		event.target.appendChild(document.getElementById(data));
+		var nodeCopy = document.getElementById(data).cloneNode(true);
+		nodeCopy.id = "newId";
+		event.target.appendChild(nodeCopy);
 		
 		carList.value = carList.value + "," + document.getElementById(data).innerHTML.trim();
 		if (carList.value.charAt(0) == ',')
@@ -46,45 +48,30 @@ var registerDragDrop = function(){
 		event.preventDefault();
 		
 	}
+	var whenDroppedOther = function(event, id){
+		event.preventDefault();
+		var id = event.dataTransfer.getData("text/html");
+		var elem = document.getElementById(id);
+		//event.target.appendChild(document.getElementById(data));
+		//var index = carList.indexOf(elem);
+		elem.parentNode.removeChild(elem);
+		//if(index > -1){
+			//carList.splice(index, 1);
+		//}
+	}
+	var whenDraggedOther = function(event){
+		event.dataTransfer.setData("text/html", event.target.id);
+	}
+	var allowDropOther = function(event){
+		event.preventDefault();
+	}
 	document.getElementById("carBox1").ondragstart = whenDragged;
 	document.getElementById("carBox2").ondragstart = whenDragged;
 	document.getElementById("carBox3").ondragstart = whenDragged;
 	document.getElementById("carPlaceDragDrop").ondragover = allowDrop;
 	document.getElementById("carPlaceDragDrop").ondrop = whenDropped;
-	
+	document.getElementById("carPlaceDragDrop").ondragstart = whenDraggedOther;
+	document.getElementById("carListDragDrop").ondragover = allowDropOther;
+	document.getElementById("carListDragDrop").ondragover = whenDroppedOther;	
 }
-
 registerDragDrop();
-
-//var idList = ["canDrag","canDrag1", "canDrag2"];
-//var elem;
-/*var registerDragDrop = function(){
-	//var dragSource;
-	
-	/*dragSource.ondragstart = function(event){
-		event.dataTransfer.setData('Text', event.target.id);
-		elem = event.target;
-		return true;
-	};
-	
-	var dropTarget = document.getElementById('listBlock');
-	dropTarget.ondrop = function(event){
-		this.value = event.dataTransfer.getData('Text');
-		//event.addElement(elem);
-		//event.target.copy(elem);
-		event.preventDefault();
-		//source.innerHTML = event.target.innerHTML;
-		//event.target.innerHTML = event.dataTransfer.getData('Text');
-		return false;
-	};
-	
-	dropTarget.ondragover = function(event){
-		event.preventDefault();
-		return false;
-	};
-	
-	dropTarget.ondragend = function(event){
-		event.preventDefault();
-		return false;
-	};*/
-//}
