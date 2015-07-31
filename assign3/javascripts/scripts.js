b7a5da4551bdddbdb64fc949ad189ad0ecc5d9a1
jaredjones@utexas.edu <jaredjones@utexas.edu@280e46b7-98d9-0841-a3c0-699020cface8>
2015-07-31 09:52:53 +0000

    Added Modal Dialog
    git-svn-id: https://agile.cs.uh.edu/svn/u15_ic/dickson_jones@2391 280e46b7-98d9-0841-a3c0-699020cface8

--- assign3/javascripts/scripts.js
+++ assign3/javascripts/scripts.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("ballGameCanvas");
 var ctx = canvas.getContext('2d');
 var mouseX = 0;
 var mouseY = 0;
-
+var gameStarted = false;
 
 var updateCanvasSizeBasedOnWindow = function()
 {
@@ -28,6 +28,26 @@ var getMousePos = function(canvas, evt) {
       x: evt.clientX - rect.left,
       y: evt.clientY - rect.top
     };
+}
+
+var roundRect = function(x, y, width, height, radius, color, alpha) {
+	ctx.globalAlpha=alpha;
+	ctx.fillStyle = color;
+	ctx.strokeStyle = color;
+	ctx.beginPath();
+	ctx.moveTo(x + radius, y);
+	ctx.lineTo(x + width - radius, y);
+	ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+	ctx.lineTo(x + width, y + height - radius);
+	ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+	ctx.lineTo(x + radius, y + height);
+	ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+	ctx.lineTo(x, y + radius);
+	ctx.quadraticCurveTo(x, y, x + radius, y);
+	ctx.closePath();
+	
+	ctx.fill();
+	ctx.globalAlpha=1.0;
 }
 
 canvas.addEventListener('mousemove', function(event) {
@@ -73,7 +93,26 @@ var drawFrame = function(){
   	
   	ctx.fillStyle = "#000000";
 	ctx.font="12px Arial";
-	ctx.fillText("Frame:" + thisFrameFPS + " : MouseX:" + mouseX + " MouseY:" + mouseY,10,50);
+	ctx.fillText("FPS:" + thisFrameFPS + " : MouseX:" + mouseX + " MouseY:" + mouseY, 10,window.innerHeight - 12);
+
+	if (!gameStarted){
+		var launchRectWidth = 400;
+		var launchRectHeight = 160;
+		var windowStartX = window.innerWidth/2 - (launchRectWidth / 2);
+		var windowStartY = window.innerHeight/2 - (launchRectHeight / 2);
+		roundRect(windowStartX, windowStartY, launchRectWidth, launchRectHeight, 10, "#0a0a0a", 0.5);
+		
+		ctx.fillStyle = "#ffffff";
+		ctx.font="22px Arial";
+		ctx.fillText("Welcome to the Bouncing Ball Game", windowStartX + 20, windowStartY + 22);
+		ctx.font="16px Arial";
+		ctx.fillText("To begin the game click anywhere on the screen to ", windowStartX + 10, windowStartY + 22 + 38);
+		ctx.fillText("spawn three balls at that location. The balls will", windowStartX + 10, windowStartY + 22 + 38 + 16);
+		ctx.fillText("move randomly in different directions. As the game", windowStartX + 10, windowStartY + 22 + 38 + 16*2);
+		ctx.fillText("progresses the balls moves faster and shrink.", windowStartX + 10, windowStartY + 22 + 38 + 16*3);
+		ctx.fillText("So be prepared, have fun, and good luck!", windowStartX + 10, windowStartY + 22 + 38 + 16*5);
+		
+	}
 
 	window.requestAnimationFrame(drawFrame);
 }
