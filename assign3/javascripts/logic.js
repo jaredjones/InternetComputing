4527f827c9d56d2f09a93941e8d853987c862e81
gteldirith543@gmail.com <gteldirith543@gmail.com@280e46b7-98d9-0841-a3c0-699020cface8>
2015-07-31 13:56:08 +0000

    Tested random function
    git-svn-id: https://agile.cs.uh.edu/svn/u15_ic/dickson_jones@2420 280e46b7-98d9-0841-a3c0-699020cface8

--- assign3/javascripts/logic.js
+++ assign3/javascripts/logic.js
@@ -1,2 +1,5 @@
 //Presently the entire game is inside of game.js, and is not unit tested
-//Once I have the game working I'll move the logic into this file and begin tests.
+//Once we have the game working we will move the logic into this file and begin tests.
+var getRandomNumberWithBounds = function(min, max) {
+  return Math.random() * (max - min) + min;
+}
--- assign3/tests/gamelogictests.js
+++ assign3/tests/gamelogictests.js
@@ -3,3 +3,11 @@ describe('canary description', function(){
         expect(true).to.be.eql(true);        
     });
 });
+describe('get random number', function(){
+	it('random number test', function(){
+		var theMin = 1;
+		var theMax = 5;
+		var range = getRandomNumberWithBounds(theMin, theMax);
+		expect(range).to.be.within(theMin,theMax);
+	});
+});
