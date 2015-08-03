472f717edeb2323214674e7fb2323c2cdc70bc05
jaredjones@utexas.edu <jaredjones@utexas.edu@280e46b7-98d9-0841-a3c0-699020cface8>
2015-08-03 18:46:47 +0000

    Prevented Max Words
    git-svn-id: https://agile.cs.uh.edu/svn/u15_ic/dickson_jones@2789 280e46b7-98d9-0841-a3c0-699020cface8

--- project/client/js/scripts.js
+++ project/client/js/scripts.js
@@ -1,6 +1,9 @@
 var wordLengthCounter = function(str){
+	if (str === undefined)
+		return 0;
 	var words = str.split(' ');
 	var currentWordLength;
+	
 	if (str.length == 0){
 		currentWordLength = 0;
 	}else{
@@ -12,15 +15,24 @@ var wordLengthCounter = function(str){
 var initalizeWorkDescriptionWordLimiter = function(){
 	var descBox = document.getElementById("work-desc");
 	var wordCounter = document.getElementById("remaining-word-counter");
-	var maxNumberOfWords = 300;
+	var maxNumberOfWords = 10;
 	
 	wordCounter.innerHTML = "Remaining Words: " + maxNumberOfWords;
 	
-	var checkLength = function(event){
+	var checkAndLimitLength = function(event){
 		var wordLength = wordLengthCounter(this.value);
+		if (wordLength > maxNumberOfWords){
+			var finalString = this.value;
+			var words = this.value.split(' ');
+			
+			words.splice(maxNumberOfWords, wordLength - maxNumberOfWords);
+			
+			descBox.value = words.join(' ');
+			wordLength = maxNumberOfWords;
+		}
 		wordCounter.innerHTML = "Remaining Words: " + (maxNumberOfWords - wordLength);
 	}
 	
-	descBox.addEventListener('keydown', checkLength);
-	descBox.addEventListener('keyup', checkLength);
+	descBox.addEventListener('keydown', checkAndLimitLength);
+	descBox.addEventListener('keyup', checkAndLimitLength);
 }
