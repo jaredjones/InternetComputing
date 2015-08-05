8edeb9557b1dcd00e0ae94842a1a2eed43a19693
gteldirith543@gmail.com <gteldirith543@gmail.com@280e46b7-98d9-0841-a3c0-699020cface8>
2015-08-05 07:04:39 +0000

    Bouncing off pad is working now.
    git-svn-id: https://agile.cs.uh.edu/svn/u15_ic/dickson_jones@3027 280e46b7-98d9-0841-a3c0-699020cface8

--- assign3/javascripts/pad.js
+++ assign3/javascripts/pad.js
@@ -5,3 +5,14 @@ var Pad = function(start, end, size, mouseX, padWidth){
 	this.mouseX = mouseX;
 	this.padWidth = padWidth;
 }
+var collidePad = function(){
+	var ball;
+	for(var i = 0; i < balls.length; i++){
+		ball = balls[i];
+		if(ball.nextY - ball.radius > 0 && ball.nextY - ball.radius <= 20 && ball.nextX >= pad.mouseX - (pad.size / 2) && ball.nextX <= pad.mouseX + (pad.size / 2) && ball.dy < 0){
+				ball.dy *= -1;
+				ball.nextY = ball.radius;
+				ball[1] = ball;
+			}
+	}
+}
