c50b787f7ec0bbe20d6a34fab0eb5c02499cd2c5
jaredjones@utexas.edu <jaredjones@utexas.edu@280e46b7-98d9-0841-a3c0-699020cface8>
2015-08-08 04:39:32 +0000

    Added Mime Test
    git-svn-id: https://agile.cs.uh.edu/svn/u15_ic/dickson_jones@3430 280e46b7-98d9-0841-a3c0-699020cface8

--- project/test/worksystemtest.js
+++ project/test/worksystemtest.js
@@ -1,6 +1,37 @@
-//var workserver = require('../src/workserver');
+var workserver = require('../src/workserver');
 
-exports.testCanary = function(test) {
-    test.strictEqual("","");
+exports.testMimeForAppCache = function(test) {
+    var mime = workserver.getMimeFromURLString("path/aardvark.appcache");
+    test.strictEqual(mime, "text/cache-manifest");
+    test.done();
+}
+
+exports.testMimeForICO = function(test) {
+    var mime = workserver.getMimeFromURLString("favicon.ico");
+    test.strictEqual(mime, "image/x-icon");
+    test.done();
+}
+
+exports.testMimeForCSS = function(test) {
+    var mime = workserver.getMimeFromURLString("aardvark.css");
+    test.strictEqual(mime, "text/css");
+    test.done();
+}
+
+exports.testMimeForJS = function(test) {
+    var mime = workserver.getMimeFromURLString("js/aardvark.js");
+    test.strictEqual(mime, "application/javascript");
+    test.done();
+}
+
+exports.testMimeForHTML = function(test) {
+    var mime = workserver.getMimeFromURLString("page.html");
+    test.strictEqual(mime, "text/html");
+    test.done();
+}
+
+exports.testMimeForRandom = function(test) {
+    var mime = workserver.getMimeFromURLString("page.lolol");
+    test.strictEqual(mime, "text/plain");
     test.done();
 }
